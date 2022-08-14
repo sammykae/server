f032951be65874eacae6ccd952512487acf2f032
sammykae <sammykay301@live.com>
2022-08-14 18:19:35 +0100

    realod

--- index.js
+++ index.js
@@ -9,32 +9,31 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
-const cors = require("cors");
-app.use(cors());
+
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
-	.connect(process.env.MONGO_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(console.log("Connected Successfully MongoDB"))
-	.catch((err) => console.log(err));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected Successfully MongoDB"))
+  .catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "images");
-	},
-	filename: (req, file, cb) => {
-		cb(null, req.body.name);
-	},
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
 
 const upload = multer({ storage: storage });
 app.post("/server/upload", upload.single("file"), (req, res) => {
-	res.status(200).json("File has been uploaded");
+  res.status(200).json("File has been uploaded");
 });
 
 app.use("/server/auth", authRoute);
@@ -46,9 +45,9 @@ app.use("/server/categories", categoryRoute);
 //   app.use(express.static("client/build"));
 //   app.get("*", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-//   });
+//   });  
 // }
 
 app.listen(PORT, () => {
-	console.log(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
